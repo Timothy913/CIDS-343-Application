c004f0629af5de561fdefa865a3a0e28022b8c10
ted <theodore.robert2003@gmail.com>
2026-02-10 20:47:51 -0600

    sent from intellij

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 let clickerButton = document.getElementById("ClickerButton");
 let counterDisplay = document.getElementById("CounterDisplay");
+let upgrade1 = document.getElementById("upgrade1");
+let upgrade2 = document.getElementById("upgrade2");
+
 
 let passiveTimeout = 3000;
 let passiveIncomeUpgrades = [];
 let count = 0;
+
+function welcome() {
+    alert ("Step into the world of digital wealth where every tap brings you closer to the ultimate payout. Our single-page clicker game allows you to go from rags to riches by clicking the dollar sign to earn crypto. Players can use their accumulated riches to purchase upgrades to including automated clickers to keep your fortune growing in while you are sleep! hover your mouse over upgrades to see the a cost and description. To earn your fortune start clicking the dollar sign!")
+}
 
 function changeMoneyCount(change) {	
 	count += change;
@@ -22,6 +29,29 @@ passiveIncome1 = new PassiveIncome(passiveIncomeUpgrades, 15, "smallGPU", "New G
 passiveIncome2 = new PassiveIncome(passiveIncomeUpgrades, 100, "roomOfGPU", "A whole room of GPUs");
 var intervalId = setInterval(function() { tickPassiveIncomeObjects(); }, 3000);
 
+
 clickerButton.addEventListener("click", function() {
 	changeMoneyCount(1);
 });
+
+upgrade1.addEventListener("click", function() {
+    if (count < 15){
+        alert("not enough crypto")
+    }else{
+    alert("success")
+    passiveIncome1.enable();
+    this.remove()
+    count = -15;
+    }
+})
+
+upgrade2.addEventListener("click", function() {
+    if (count < 100){
+        alert("not enough Crypto")
+    }else{
+    alert("success")
+    passiveIncome2.enable();
+    this.remove()
+    count = -100;
+    }
+})
